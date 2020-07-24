f2461aad66c2d046d5e36d096891829b1abec0ab
Matheus Neumann <tetteu777@gmail.com>
2020-07-23 22:50:13 -0300

    test1

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -33,7 +33,7 @@ class HomePage extends StatelessWidget {
           builder: (_) {
             return Stack(
               children: [
-                Background(Colors.black, outroMetodo),
+                Background(Colors.yellow, outroMetodo),
                 Container(
                   width: 328.w,
                   height: 320.h,
