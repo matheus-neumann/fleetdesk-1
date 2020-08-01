da0805d10ef02edf3ab3f4abf1b48dd4fa9feabe
Matheus Neumann <tetteu777@gmail.com>
2020-08-01 14:46:38 -0300

    refactored widgets

--- lib/app/ui/android/login/login_page.dart
+++ lib/app/ui/android/login/login_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:fleetdesk/app/controller/login/login_controller.dart';
 import 'package:fleetdesk/app/controller/splash/splash_controller.dart';
-import 'package:fleetdesk/app/ui/android/login/widgets/bottom_card_widget.dart';
-import 'package:fleetdesk/app/ui/android/login/widgets/top_card_widget.dart';
+import 'package:fleetdesk/app/ui/android/widgets/bottom_card_widget.dart';
+import 'package:fleetdesk/app/ui/android/widgets/top_card_widget.dart';
 import 'package:slimy_card/slimy_card.dart';
 
 class LoginPage extends GetView<DetailsController> {
