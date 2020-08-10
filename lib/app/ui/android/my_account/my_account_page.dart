52fa3c367f1f04e128c127ece85d7be607fc0af3
sergioludvig <sergioludvig@hotmail.com>
2020-08-10 13:48:07 -0300

    add pages and widgets

--- lib/app/ui/android/my_account/my_account_page.dart
+++ lib/app/ui/android/my_account/my_account_page.dart
@@ -17,129 +17,326 @@ class MyAccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     ScreenUtil.init(context, width: 360, height: 640, allowFontScaling: true);
     return Scaffold(
-      body: SingleChildScrollView(
-          child: SafeArea(
-              child: Padding(
-                padding:
-                EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        backgroundColor: splashColor,
+        body: SingleChildScrollView(
+            child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Stack(children: <Widget>[
+              Container(
+                  height: 67.5.h,
+                  width: 360.w,
+                  decoration: BoxDecoration(
+                      color: appBarColor,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0)))),
+              Container(
+                margin: EdgeInsets.only(top: 19.5.h, bottom: 16.h),
+                height: 32.h,
+                width: 360.w,
                 child: Stack(
-                  children: <Widget>[
+                  children: [
+                    GestureDetector(
+                        child: Container(
+                            margin: EdgeInsets.only(
+                                left: 16.w,
+                                right: 334.w,
+                                top: 8.h,
+                                bottom: 8.h),
+                            width: 10.w,
+                            child: Icon(Icons.arrow_back_ios,
+                                color: Color(0xffffffff))),
+                        onTap: () {
+                          Get.toNamed(Routes.MENU);
+                        }),
                     Container(
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [Color(0xff0E1828), Color(0xff090E19)])),
-                        child: ContainerForm(height: 346.h, top: 96.h, bottom: 198.h)),
-                    MenuButton(left: 31.w, top: 113.h, right: 34.w, bottom: 492.h, text: 'Minha Conta',icon: Icons.person),
-                    MenuButton(left: 31.w, top: 160.h, right: 34.w, bottom: 445.h, text: 'Tarefas',icon: Icons.assignment),
-                    MenuButton(left: 31.w, top: 207.h, right: 34.w, bottom: 398.h, text: 'Ranking',icon: Icons.trending_up),
-                    MenuButton(left: 31.w, top: 254.h, right: 34.w, bottom: 351.h, text: 'Alertas',icon: Icons.notifications),
-                    MenuButton(left: 31.w, top: 301.h, right: 34.w, bottom: 304.h, text: 'Mensagens',icon: Icons.mode_comment),
-                    MenuButton(left: 31.w, top: 348.h, right: 34.w, bottom: 257.h, text: 'Configuracoes',icon: Icons.settings),
-                    MenuButton(left: 31.w, top: 395.h, right: 34.w, bottom: 210.h, text: 'Bluetooth',icon: Icons.bluetooth),
-                    Container(
-                      height: 41.h,
-                      width: 162.w,
-                      margin: EdgeInsets.only(
-                          top: 26.h, bottom: 573.h, right: 180.w, left: 18.w),
+                      width: 120.w,
+                      height: 30.h,
+                      margin:
+                          EdgeInsets.only(left: 128.w, top: 2.h, right: 112.w),
                       child: FittedBox(
+                        // TODO - Verify size and position of text
+                        child: Text(
+                          'Minha conta',
+                          style: TextStyle(
+                              fontSize: 18.sp,
+                              color: Colors.white,
+                              fontFamily: 'Montserrat'),
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                        child: Container(
+                            margin: EdgeInsets.only(
+                                left: 320.w,
+                                right: 16.w,
+                                top: 4.h,
+                                bottom: 4.h),
+                            width: 10.w,
+                            child: Icon(Icons.edit, color: Color(0xffffffff))),
+                        onTap: () {
+                          Get.toNamed(Routes.MENU);
+                        }),
+                  ],
+                ),
+              ),
+              Container(
+                height: 40.h,
+                width: 162.w,
+                margin: EdgeInsets.only(
+                    left: 16.w, top: 83.h, right: 182.w, bottom: 1053.h),
+                child: FittedBox(
+                  alignment: Alignment.centerLeft,
+                  // TODO - Review size and position of text
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Olá,\n',
+                      style: TextStyle(
+                          fontSize: 16.sp,
+                          color: Colors.white,
+                          fontFamily: 'Montserrat'),
+                      /*defining default style is optional */
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: 'Ricardo Gomes',
+                            style: TextStyle(
+                                fontSize: 22.sp,
+                                color: Colors.white,
+                                fontFamily: 'MontserratBold')),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 240.w,
+                height: 84.h,
+                margin: EdgeInsets.only(
+                    left: 15.w, top: 140.h, right: 105.w, bottom: 953.h),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 84.0,
+                      height: 84.0,
+                      decoration: new BoxDecoration(
+                        borderRadius: new BorderRadius.circular(42.0),
+                        border: new Border.all(
+                          width: 1.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Icon(
+                          Icons.add_photo_alternate,
+                          size: 36.sp,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20.w,
+                    ),
+                    Container(
+                      height: 32.h,
+                      width: 136.w,
+                      child: FittedBox(
+                        fit: BoxFit.cover,
                         alignment: Alignment.centerLeft,
                         // TODO - Review size and position of text
                         child: RichText(
                           text: TextSpan(
-                            text: 'Ola,\n',
+                            text: 'Envie uma foto sua\n',
                             style: TextStyle(
                                 fontSize: 16.sp,
                                 color: Colors.white,
-                                fontFamily: 'Montserrat'
-                            ),
+                                fontFamily: 'MontserratSemiBold'),
                             /*defining default style is optional */
                             children: <TextSpan>[
                               TextSpan(
-                                  text: 'Ricardo Gomes',
+                                  text: 'Foto estilo selfie.',
                                   style: TextStyle(
-                                      fontSize: 22.sp,
+                                      fontSize: 16.sp,
                                       color: Colors.white,
-                                      fontFamily: 'MontserratBold'
-                                  )),
+                                      fontFamily: 'Montserrat')),
                             ],
                           ),
                         ),
                       ),
                     ),
-                    Container(
-                      height: 24.h,
-                      width: 132.w,
-                      margin: EdgeInsets.only(
-                          top: 465.h, bottom: 151.h, right: 116.w, left: 114.w),
-                      child: Text(
-                        'Placa do veiculo',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.sp,
-                            fontFamily: 'Montserrat'
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 24.h,
-                      width: 132.w,
-                      margin: EdgeInsets.only(
-                          top: 485.h, bottom: 132.h, right: 143.w, left: 139.w),
-                      child: Text(
-                        'IJSH8JSG',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'MontserratBold'
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(
-                          top: 524.h, bottom: 92.h, right: 153.w, left: 149.w),
-                      child: GestureDetector(
-                        child: Text(
-                          'Logout',
-                          // TODO - Adjust font size, font weight and configure to navigate for 'Recovery Password' page
-                          style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            fontSize: 16.sp,
-                            color: Colors.white,
-                          ),
-                        ),
-                        onTap: () {
-                          Get.toNamed(Routes.LOGIN);
-                        },
-                      ),
-                    ),
-                    //),
                   ],
                 ),
-              ))),
-      bottomNavigationBar: Container(
-        height: 64.h,
-        width: 360.w,
-        child: BottomNavigationBar(
-          currentIndex: 0, // this will be set when a new tab is tapped
-          items: [
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.home),
-              title: new Text('Home'),
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.mail),
-              title: new Text('Messages'),
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                title: Text('Profile')
-            ),
-          ],
-        ),
-      ),
-    );
+              ),
+              ContainerForm(
+                height: 803.h,
+                top: 245.h,
+                bottom: 129.h,
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    top: 269.h, bottom: 763.h, right: 142.w, left: 32.w),
+                child: FittedBox(
+                  // TODO - Verify size and position of text
+                  child: Text(
+                    'Cadastro de Motorista',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.sp,
+                        color: red,
+                        fontFamily: 'MontserratBold'),
+                  ),
+                ),
+              ),
+              Container(
+                height: 48.h,
+                width: 296.w,
+                margin: EdgeInsets.only(
+                    top: 301.h, bottom: 844.h, right: 98.w, left: 32.w),
+                child: FittedBox(
+                  child: Text(
+                    'Se precisar, edite ou complete as\ninformações abaixo.',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'Montserrat'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 32.w, top: 349.h, right: 284.w, bottom: 812.h),
+                child: FittedBox(
+                  child: Text(
+                    'Nome',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'MontserratSemiBold'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 32.w, top: 425.h, right: 244.w, bottom: 736.h),
+                child: FittedBox(
+                  child: Text(
+                    'Sobrenome',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'MontserratSemiBold'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 32.w, top: 501.h, right: 299.w, bottom: 660.h),
+                child: FittedBox(
+                  child: Text(
+                    'CPF',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'MontserratSemiBold'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 32.w, top: 577.h, right: 165.w, bottom: 584.h),
+                child: FittedBox(
+                  child: Text(
+                    'Carteira de Habilitação',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'MontserratSemiBold'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 32.w, top: 349.h, right: 284.w, bottom: 812.h),
+                child: FittedBox(
+                  child: Text(
+                    'Nome',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'MontserratSemiBold'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 32.w, top: 653.h, right: 305.w, bottom: 508.h),
+                child: FittedBox(
+                  child: Text(
+                    'PIS',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'MontserratSemiBold'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 32.w, top: 729.h, right: 182.w, bottom: 432.h),
+                child: FittedBox(
+                  child: Text(
+                    'Data de Nascimento',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'MontserratSemiBold'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 32.w, top: 805.h, right: 282.w, bottom: 356.h),
+                child: FittedBox(
+                  child: Text(
+                    'E-mail',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'MontserratSemiBold'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 32.w, top: 881.h, right: 267.w, bottom: 280.h),
+                child: FittedBox(
+                  child: Text(
+                    'Telefone',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'MontserratSemiBold'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                    left: 32.w, top: 957.h, right: 233.w, bottom: 204.h),
+                child: FittedBox(
+                  child: Text(
+                    'Alterar senha',
+                    style: TextStyle(
+                        fontSize: 14.sp,
+                        color: black,
+                        fontFamily: 'MontserratSemiBold'),
+                  ),
+                ),
+              ),
+              LoginButton(1083.h, 46.h, "Salvar", null)
+            ]),
+          ),
+        )));
   }
 }
