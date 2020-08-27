ccbe03c4cbc048c1239c75ed9b236cf6d312c03b
sergioludvig <sergioludvig@hotmail.com>
2020-08-27 15:06:51 -0300

    created alerts and messages page design

---
+++ lib/app/ui/android/widgets/alerts_widgets.dart
@@ -0,0 +1,104 @@
+import 'package:fleetdesk/app/routes/app_pages.dart';
+import 'package:fleetdesk/app/ui/android/tasks/tasks_info_page.dart';
+import 'package:fleetdesk/app/ui/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class AlertsContainer extends StatelessWidget {
+
+  String title;
+  String text;
+  String date;
+
+  AlertsContainer({this.title, this.text, this.date});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 122.h,
+      width: 328.w,
+      margin: EdgeInsets.only(left: 16.w),
+      child: Container(
+        //height: 117.h,
+        width: 328.w,
+        decoration: BoxDecoration(
+          color: Color(0xff1D2634),
+          borderRadius: BorderRadius.circular(
+              20), // TODO - Verify radius of Container Fields
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 12.h,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(left: 8.w),
+              child: Text(
+                "Nova solicitação de entrega",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5.h,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(left: 8.w),
+              child: Text(
+                "Lorem ipsum dolor sit amet, consectetuer\nadipiscing elit. Aenean commodo ligula\neget dolor. Aenean massa.",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.sp,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Container( margin: EdgeInsets.only(left: 8.w), alignment: Alignment.centerLeft,child: CustomPaint(painter: LineDashedPainter(),)),
+            SizedBox(
+              height: 10.h,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(left: 244.w),
+              child: Text(
+                "18:36 | 14/07/20",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10.sp,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LineDashedPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()..strokeWidth = 1;
+    var max = 301.w;
+    var dashWidth = 5;
+    var dashSpace = 5;
+    double startX = 0;
+    while (max >= 0) {
+      canvas.drawLine(Offset(startX, 0), Offset(startX + dashWidth, 0), paint);
+      final space = (dashSpace + dashWidth);
+      startX += space;
+      max -= space;
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
--- lib/app/ui/messages.dart
+++ lib/app/ui/messages.dart
@@ -1,6 +1,9 @@
+import 'package:fleetdesk/app/routes/app_pages.dart';
 import 'package:fleetdesk/app/ui/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 import 'android/widgets/navigation_bottom_bar.dart';
 
@@ -9,40 +12,234 @@ class MessagesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     ScreenUtil.init(context, width: 360, height: 640, allowFontScaling: true);
     return Scaffold(
-      bottomNavigationBar: BottomNavBar(currentIndex: 1,),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: 1,
+      ),
       backgroundColor: splashColor,
       body: SingleChildScrollView(
         child: SafeArea(
           child: Padding(
             padding: EdgeInsets.only(
                 bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: Container(
-              child: Column(
-                children: [
-                  Container(
-                    width: 312.w,
-                    height: 137.h,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(20.sp),
-                          topLeft: Radius.circular(20.sp),
-                          bottomLeft: Radius.circular(20.sp),
-                        )),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 21.h,
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 16.w),
+                  child: Row(
+                    children: [
+                      InkWell(
+                          child: Container(
+                              width: 10.w,
+                              child: Icon(Icons.arrow_back_ios,
+                                  color: Color(0xffffffff))),
+                          onTap: () {
+                            Get.toNamed(Routes.MENU);
+                          }),
+                      SizedBox(
+                        width: 107.w,
+                      ),
+                      Text(
+                        'Mensagens',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.sp,
+                            color: Colors.white,
+                            letterSpacing: 0.3.sp),
+                      ),
+                    ],
                   ),
-                  Container(
-                    width: 312.w,
-                    height: 137.h,
-                    decoration: BoxDecoration(
-                        color: red,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(20.sp),
-                          topLeft: Radius.circular(20.sp),
-                          bottomRight: Radius.circular(20.sp),
-                        )),
-                  )
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 27.h,
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  margin: EdgeInsets.only(left: 38.w),
+                  width: 312.w,
+                  height: 137.h,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20.sp),
+                        topLeft: Radius.circular(20.sp),
+                        bottomLeft: Radius.circular(20.sp),
+                      )),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 10.w, top: 10.h),
+                        child: Row(
+                          children: [
+                            SvgPicture.asset('assets/images/profile_icon_example.svg', color: black,),
+                            SizedBox(
+                              width: 8.w,
+                            ),
+                            Text('Carlos Amaral', style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 9.h,
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.only(left: 10.w),
+                        child: Text("Lorem ipsum dolor sit amet,\nconsectetuer adipiscing elit enean\ncommodo ligula eget dolor.\nAenean massa.",
+                        style: TextStyle(fontSize: 16.sp),),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 3.h,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 300.w,
+                    ),
+                    Text(
+                      '20:29',
+                      style: TextStyle(fontSize: 12.sp, color: Colors.white),
+                    ),
+                    SizedBox(
+                      width: 8.w,
+                    ),
+                    Icon(
+                      Icons.check,
+                      color: Colors.white,
+                      size: 12.sp,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                Container(
+                  width: 312.w,
+                  height: 137.h,
+                  margin: EdgeInsets.only(right: 16.w),
+                  decoration: BoxDecoration(
+                      color: red,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20.sp),
+                        topLeft: Radius.circular(20.sp),
+                        bottomRight: Radius.circular(20.sp),
+                      )),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 10.w, top: 10.h),
+                        child: Row(
+                          children: [
+                            SvgPicture.asset('assets/images/profile_icon_example.svg', color: Colors.white,),
+                            SizedBox(
+                              width: 8.w,
+                            ),
+                            Text('Central de Operacional', style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.bold, color: Colors.white),)
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 9.h,
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.only(left: 10.w),
+                        child: Text("Lorem ipsum dolor sit amet,\nconsectetuer adipiscing elit enean\ncommodo ligula eget dolor.\nAenean massa.",
+                          style: TextStyle(fontSize: 16.sp, color: Colors.white),),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 3.h,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 16.w,
+                    ),
+                    Text(
+                      '20:29',
+                      style: TextStyle(fontSize: 12.sp, color: Colors.white),
+                    ),
+                    SizedBox(
+                      width: 8.w,
+                    ),
+                    Stack(
+                      children: [
+                        Icon(
+                          Icons.check,
+                          color: Colors.white,
+                          size: 12.sp,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 7.w),
+                          child: Icon(
+                            Icons.check,
+                            color: Colors.white,
+                            size: 12.sp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 91.h),
+                      width: 360.w,
+                      height: 96.h,
+                      decoration: BoxDecoration(
+                          color: Color(0XFF1D2634),
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(20.sp),
+                              topLeft: Radius.circular(20.sp))),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 12.w,
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(bottom: 22.h),
+                            width: 256.w,
+                            height: 40.h,
+                            decoration: BoxDecoration(
+                                color: Color(0XFFF2F2F2),
+                                borderRadius: BorderRadius.circular(10.sp)),
+                            child: Container(
+                              margin: EdgeInsets.only(left: 12.w, top: 12.h),
+                              child: Text(
+                                'Escreva sua mensagem',
+                                style: TextStyle(
+                                  fontSize: 14.sp,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 16.w,
+                          ),
+                          Container(
+                              margin: EdgeInsets.only(bottom: 22.h),
+                            child: InkWell(onTap: null,child: SvgPicture.asset('assets/images/camera_icon.svg')),),
+                          SizedBox(
+                            width: 16.w,
+                          ),
+                          Container(
+                              margin: EdgeInsets.only(bottom: 22.h),
+                              child: InkWell(onTap: null, child: SvgPicture.asset('assets/images/mic_icon.svg')),),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
